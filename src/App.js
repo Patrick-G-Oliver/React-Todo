52f4195994d02328b0026b799711cfe1b8811766
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-05 20:56:32 -0400

    App built out

--- src/App.js
+++ src/App.js
@@ -3,29 +3,70 @@ import React from 'react';
 const todos = [
   {
     task: "bake bread",
-    id: 1,
+    id: Date.now(),
     completed: false
   },
   {
     task: "mow lawn",
-    id: 2,
+    id: Date.now(),
     completed: false
   },
   {
     task: "buy groceries",
-    id: 3,
+    id: Date.now(),
     completed: false
   }
-]
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+  };
+
+  toggleTodo = todoId => {
+    console.log(todoId);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todoId === todo.id) {
+          return {
+            ...todo, 
+            done: !todo.done
+          };
+        };
+      })
+    });
+  };
+
+  addTodo = (event, todo) => {
+    event.preventDefault();
+    const newTask = {
+      task: todo,
+      id: Date.now(),
+      done: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTask]
+    });
+  };
+
+  clearDoneTodos = event => {
+    event.preventDefault();
+    this.setState({
+      todos: this.state.todos.filter(todo => !todo.done)
+    });
+  };
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>Things to do...</h1>
       </div>
     );
   }
